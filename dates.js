ce2c4f2b6003191b5cb16624e7ff45280d399ea7
Sheryl Boughton <sherpbo@gmail.com>
2015-05-15 19:45:00 -0700

    this might be a little verbose, buuuut....

--- dates.js
+++ dates.js
@@ -1,14 +1,43 @@
-var today = new Date(2015, 3, 2);
 
-function age(year, month, day) {
-  var results = {};
-  results.days = Math.abs(day - today.getDate());
-  var monthsDiff = Math.abs(month - today.getMonth());
-  var yearsDiff = Math.abs(year - today.getFullYear());
-  results.months = monthsDiff === 0 ? undefined : monthsDiff;
-  results.years = yearsDiff === 0 ? undefined : yearsDiff;
-  return results;
+// Write a JavaScript function that takes 3 arguments, year, month, and day, and calculates how many days between that day and today
+//
+// If the days are more than 30, return Y months, Z days
+// If the days are months are more than 12, return X years, Y months, Z days
+// Test Cases
+//
+// (Assuming today is April 2nd, 2015)
+//
+// age(2017, 11, 17) -> 2 years, 8 months, 15 days
+//
+// age(2015, 3, 6) -> 4 days
+// Note that the month argument starts from 0 (11 is December)
 
+function age(year, month, day){
+  // var thisYear = new Date().getFullYear();
+  // var thisMonth = new Date().getMonth();
+  // var today = new Date().getDate();
+
+  var now = {thisYear: new Date().getFullYear(), thisMonth: new Date().getMonth(), date: new Date().getDate()};
+  var years =  now.thisYear - year;
+  console.log(years);
+  var months =  now.thisMonth - month;
+  console.log(months);
+  var days = now.date - day;
+  console.log(days);
+
+  if(days>30 && months>11){
+    return "years + ' years'";
+  } else if(days>30){
+    return "months + ' months,' + years + ' years'";
+  } else{
+    return "days + ' days,' + months + ' months,' + years + ' years'";
+  }
 }
 
-// ^^^ don't use this! just an example! ^^^ //
+
+
+
+
+
+
+age(2017, 1, 7);
--- jquery-dom.js
+++ jquery-dom.js
@@ -1,7 +1,44 @@
-$( "div" ).each(function( index ) {
-  // console.log( index + ": " + $( this ).text() );
-  var random = function(){
+function invertColor(hexTripletColor) {
+  var color = hexTripletColor;
+  color = color.substring(1);           // remove #
+  color = parseInt(color, 16);          // convert to integer
+  color = 0xFFFFFF ^ color;             // invert three bytes
+  color = color.toString(16);           // convert to hex
+  color = ("000000" + color).slice(-6); // pad with leading zeros
+  color = "#" + color;                  // prepend #
+  return color;
+}
 
-  };
-  index.css('background-color', random)
+function rgb2hex(rgb){
+  rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+  return (rgb && rgb.length === 4) ? "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+}
+
+$(document).ready(function(){
+  var color;
+
+  $('div').each(function(index, element){
+    var randoms = [];
+    for (var i = 1; i<=3; i++){
+      randoms.push(Math.floor(Math.random()*255));
+    }
+    //   console.log(randoms);
+    // var rgbNumbers = randoms.join(' ');
+
+    color = 'rgb(' + randoms.join(', ') + ')';
+    console.log(color);
+
+    $(element).css('background-color', color);
+  });
+
+  $('div').on('click', function(){
+    console.log('clicked');
+    var hex = rgb2hex(color);
+    console.log(hex);
+    $(this).css('background-color', invertColor(hex));
+  });
+
 });
