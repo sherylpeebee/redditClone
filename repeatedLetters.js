944c81d29db18f0295ff188be3f254e513d23a8b
Sheryl Boughton <sherpbo@gmail.com>
2015-05-21 18:03:17 -0700

    getting closer, and less weird, i think

--- repeatedLetters.js
+++ repeatedLetters.js
@@ -1,4 +1,4 @@
- // Write a JavaScript function that takes a sentence as an argument and determines which word in that sentence has the greatest number of repeated letters.
+// Write a JavaScript function that takes a sentence as an argument and determines which word in that sentence has the greatest number of repeated letters.
 //
 // If the sentence has multiple words with the same max of repeated letters, return them all in an Array.
 //
@@ -10,36 +10,20 @@
 // ["attribute", "success"]
 
 function wordSelector(sentence){
+ var matches = [];
+ var topWords = {};
 
-  var matches = [];
-var sentence = 'eggs, ham, cheese';
-var split1 = sentence.split(', ');
+var split1 = sentence.split(' ');
 console.log(split1);
+ split1.forEach(function(e){
+   for(var i=0; i<e.length; i++){
+     if(e[i]===e[i-1]){
+     matches.push(e, e[i]===e[i-1]);
+     }
+   }
+ });
 
-var split2 = function(array){
-  var arr = [];
-  array.forEach(function(element){
-    arr.push(element.split(''));
-  });
-  return arr;
-};
-
-
-var arrofArr = split2(split1);
-// console.log(arrofArr);
-var library = [];
-for(var i=0; i<arrofArr.length; i++){
-  console.log(i + ':' + arrofArr[i]);
-  arrofArr[i].forEach(function(letter){
-    library.push({letter: 0});
-    console.log(library);
-  });
-}
-
-
-
-
-
+ console.log(matches);
 }
 
 wordSelector("I attribute my success to this: I never gave or took any excuse. –Florence Nightingale");
