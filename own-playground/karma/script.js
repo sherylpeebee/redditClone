a95e6cd7e28808fb3cbfc5598412084135a5fe7b
Sheryl Boughton <sherpbo@gmail.com>
2015-06-10 14:06:01 -0700

    includes success and error handlers; better practice where asynchrony is involved

---
+++ own-playground/karma/script.js
@@ -0,0 +1,37 @@
+
+var leaderboard2 = function(data) {
+  return data.sort(function(a,b){return b.points-a.points;});
+};
+
+function liCreate(name,points) {
+  var li = $('<li>'+name+'</li>');
+  li.append('<span>'+points+'</span>');
+}
+
+$(document).ready(function() {
+  // var sorted = leaderboard2(data);
+  // for (var i=0; i<sorted.length; i++) {
+  //   $('body').append('<div><p>'+sorted[i].name+' '+sorted[i].points+'</p></div>')
+  // }
+  // //problem here is with repeated DOM manipulation
+
+  var studentArray = [];
+  $.getJSON('fake-api.json').success(function(data){
+    //using '$.getJSON' as opposed to $.ajax specifies
+    //also, include both success and error handler to account for asynchrony.
+    //i.e., if/when SUCCESS, execute some code block; if ERROR, execute another.
+    console.log(data);
+    var sorted = leaderboard2(data);
+    for (var i=0; i<sorted.length; i++) {
+      var student = ('<div><p>'+sorted[i].name+' '+sorted[i].points+'</p></div>');
+      studentArray.push(student);
+    }
+    //^^ FIXED!!! Append entire array ^^
+    console.log(studentArray);
+    $('body').append(studentArray);
+
+  })
+    .error(function(error){
+      console.log(error);
+    });
+});
