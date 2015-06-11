c90ce900c102dfd52c783d5703988e521caa2832
Sheryl Boughton <sherpbo@gmail.com>
2015-06-11 14:48:37 -0700

    donezo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var doubler = function(a, b) {
-}
+return a*2 + b*2;
+};
 
-console.assert(doubler(4,2) === 12);
-console.assert(doubler(1,3) === 8);
-console.assert(doubler(-1,3) === 4);
+console.log(doubler(4, 2) === 12);
+console.log(doubler(1, 3) === 8);
+console.log(doubler(-1, 3) === 4);
+
+//console.assert didn't work in terminal
